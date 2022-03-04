320ae8edb15961186d650de0258049f3b8882c27
sunzg <1195727432@qq.com>
2022-03-04 11:09:17 +0800

    //add proxy

--- src/main/java/sun/zhao/guo/javaguide/dynamicagent/jdk/SmsServiceTest.java
+++ src/main/java/sun/zhao/guo/javaguide/dynamicagent/jdk/SmsServiceTest.java
@@ -4,7 +4,8 @@ public class SmsServiceTest {
 
     public static void main(String[] args) {
         SmsService smsService = (SmsService) JdkProxyFactory.getProxy(new SmsServiceImpl());
-        smsService.sendMsg("hello, sunzhaoguo");
+        String msg = smsService.sendMsg("hello, sunzhaoguo");
+        System.out.println("msg = " + msg);
     }
 
 }
