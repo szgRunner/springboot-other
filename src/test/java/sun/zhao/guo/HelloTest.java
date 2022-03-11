94deac946f0d5aa471e7c053f3e27fb54cb3ee8c
sunzg <1195727432@qq.com>
2022-03-11 17:35:53 +0800

    //add 线程池 + 同步锁

--- src/test/java/sun/zhao/guo/HelloTest.java
+++ src/test/java/sun/zhao/guo/HelloTest.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class HelloTest {
@@ -12,6 +16,19 @@ public class HelloTest {
     @Test
     public void test(){
         System.out.println("true = " + true);
+    }
+
+    @Test
+    public void test2(){
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(Instant.now());
+            }
+        }, 10000L, 3000L);
+
     }
 
 }
