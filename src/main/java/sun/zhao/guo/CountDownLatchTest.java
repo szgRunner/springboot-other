2bffa2d64c9e29d7288fb3c411fbdcf19518bc9d
sunzg <1195727432@qq.com>
2024-08-01 17:05:31 +0800

    // CountDownLatchTest.java // by sunzg

---
+++ src/main/java/sun/zhao/guo/CountDownLatchTest.java
@@ -0,0 +1,49 @@
+package sun.zhao.guo;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Package: sun.zhao.guo
+ * @Author: SzgStart-sunzg
+ * @Date: 2024/08/01/10:18
+ * @Description:
+ */
+public class CountDownLatchTest {
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        final CountDownLatch cdOrder = new CountDownLatch(1);
+        final CountDownLatch cdAnswer = new CountDownLatch(4);
+        for (int i = 0; i < 4; i++) {
+            service.execute(() -> {
+                try {
+                    System.out.println("选手" + Thread.currentThread().getName() + "正在等待裁判发布口令");
+                    cdOrder.await();
+                    System.out.println("选手" + Thread.currentThread().getName() + "已接受裁判口令");
+                    Thread.sleep((long) (Math.random() * 10000));
+                    System.out.println("选手" + Thread.currentThread().getName() + "到达终点");
+                    cdAnswer.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        try {
+            Thread.sleep((long) (Math.random() * 10000));
+            System.out.println("裁判"+Thread.currentThread().getName()+"即将发布口令");
+            cdOrder.countDown();
+            System.out.println("裁判"+Thread.currentThread().getName()+"已发送口令，正在等待所有选手到达终点");
+            cdAnswer.await();
+            System.out.println("所有选手都到达终点");
+            System.out.println("裁判"+Thread.currentThread().getName()+"汇总成绩排名");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+
+}
